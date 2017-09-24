eb5de51de1416246e61aa3d702f28ccd9f2ccf49
alexandr.gorbenko <alexandr1104@gmail.com>
2017-09-24 19:38:33 +0300

    update seeds

--- database/seeds/CommentSeeder.php
+++ database/seeds/CommentSeeder.php
@@ -12,26 +12,43 @@ class CommentSeeder extends Seeder
     public function run()
     {
 
-        $comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-        $commentLength = strlen($comment);
-
         DB::table('comments')->insert([
             [
                 'id' => 1,
                 'parent_id' => 0,
-                'text' => substr($comment, 0, rand(5, $commentLength)),
+                'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 'path' => '$1.',
+                'level' => 0,
                 'created_at' => date('Y-m-d H:i:s', time()),
                 'updated_at' => date('Y-m-d H:i:s', time()),
             ],
             [
                 'id' => 2,
                 'parent_id' => 0,
-                'text' => substr($comment, 0, rand(5, $commentLength)),
+                'text' => 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 'path' => '$2.',
+                'level' => 0,
                 'created_at' => date('Y-m-d H:i:s', time()),
                 'updated_at' => date('Y-m-d H:i:s', time()),
             ],
+            [
+                'id' => 3,
+                'parent_id' => 1,
+                'text' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                'path' => '$1.$3.',
+                'level' => 1,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        [
+            'id' => 4,
+            'parent_id' => 1,
+            'text' => 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'path' => '$1.$3.$4.',
+            'level' => 2,
+            'created_at' => date('Y-m-d H:i:s', time()),
+            'updated_at' => date('Y-m-d H:i:s', time()),
+        ],
         ]);
     }
 }

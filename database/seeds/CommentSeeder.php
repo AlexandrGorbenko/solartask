4a928bb45b6e31ec66848703b0455648fb1bf835
alexandr.gorbenko <alexandr1104@gmail.com>
2017-09-24 11:17:51 +0300

    Add migration

---
+++ database/seeds/CommentSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        $commentLength = strlen($comment);
+
+        DB::table('comments')->insert([
+            [
+                'id' => 1,
+                'parent_id' => 0,
+                'text' => substr($comment, 0, rand(5, $commentLength)),
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'id' => 2,
+                'parent_id' => 0,
+                'text' => substr($comment, 0, rand(5, $commentLength)),
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ]);
+    }
+}
